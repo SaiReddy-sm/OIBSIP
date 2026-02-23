322dd9e416ec6ed50193030aa9c97cfa99d7b8d7
SaiReddy-sm <238x1a4213@khitguntur.ac.in>
2026-02-23 20:54:25 +0530

    Add typing effect, counter animation, and smooth scroll

---
+++ task2-portfolio/script.js
@@ -0,0 +1,38 @@
+// Typing Animation
+const typingText = "Frontend Developer | Full Stack Developer";
+let index = 0;
+
+function typeEffect() {
+  if (index < typingText.length) {
+    document.getElementById("typing").innerHTML += typingText.charAt(index);
+    index++;
+    setTimeout(typeEffect, 50);
+  }
+}
+typeEffect();
+
+
+// Counter Animation
+function animateCounter(id, target) {
+  let count = 0;
+  let interval = setInterval(() => {
+    count++;
+    document.getElementById(id).innerText = count + "+";
+    if (count === target) {
+      clearInterval(interval);
+    }
+  }, 50);
+}
+
+animateCounter("exp", 1);
+animateCounter("projectsDone", 6);
+
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
